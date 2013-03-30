e70b52412473cdc9a5877817e4f499a726beb2bd
Yannick Signer <thasiggi1@gmail.com>
2013-03-30 18:20:34 +0100

    better look
    better look

--- bin/tree.dart
+++ bin/tree.dart
@@ -14,12 +14,36 @@ void main() {
         File file = value;
         Path p = new Path(file.path);
         String s = p.filename;
-        print(" \\-$s");
+        StringBuffer sb = new StringBuffer();
+        
+        Iterable<Match> matchs = new RegExp("/").allMatches(p.toString());
+        int id = matchs.length;
+        for (int i = 1; i < id; i++) {
+          sb.write(" ");
+        }
+        sb.write("\\-$s");
+        
+        String finalString = sb.toString();
+        print("$finalString");
+      
       } else if(value is Directory){
         Directory d = value;
         Path p = new Path(d.path);
         String s = d.path;
-        print("-$s");
+        String name = p.filename;
+        Iterable<Match> matchs = new RegExp("/").allMatches(s);
+        int id = matchs.length;
+        
+        StringBuffer sb = new StringBuffer();
+        
+        for (int i = 0; i < id; i++) {
+          sb.write(" ");
+        }
+        sb.write("$name");
+
+        String finalString = sb.toString();
+        
+        print("$finalString");
       }
     });
   }
