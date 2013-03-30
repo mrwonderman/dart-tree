7d861c0f16e5c210c546f1d911fec13e8cc34aea
Yannick Signer <thasiggi1@gmail.com>
2013-03-30 18:40:37 +0100

    cleanup
    a bit of cleanup

--- bin/tree.dart
+++ bin/tree.dart
@@ -3,8 +3,9 @@ import 'dart:async';
 
 void main() {
   List<String> arguments = (new Options()).arguments;
+  
   if(arguments.isEmpty){
-  print("no directory selected.. \ntry : ./dart.exe tree.dart folder");
+    print("no directory selected.. \ntry : ./dart.exe tree.dart folder");
   } else {
     Directory dir = new Directory.fromPath(new Path(arguments.first));
     print(dir.path);
@@ -15,14 +16,12 @@ void main() {
         Path p = new Path(file.path);
         String s = p.filename;
         StringBuffer sb = new StringBuffer();
-        
         Iterable<Match> matchs = new RegExp("/").allMatches(p.toString());
         int id = matchs.length;
         for (int i = 1; i < id; i++) {
           sb.write(" ");
         }
         sb.write("\\-$s");
-        
         String finalString = sb.toString();
         print("$finalString");
       
@@ -33,16 +32,12 @@ void main() {
         String name = p.filename;
         Iterable<Match> matchs = new RegExp("/").allMatches(s);
         int id = matchs.length;
-        
         StringBuffer sb = new StringBuffer();
-        
         for (int i = 0; i < id; i++) {
           sb.write(" ");
         }
         sb.write("$name");
-
         String finalString = sb.toString();
-        
         print("$finalString");
       }
     });
